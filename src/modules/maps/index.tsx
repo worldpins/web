6ca525b7ee9e423a9c7fa96b551fed954611e8eb
jdecroock <decroockjovi@gmail.com>
2019-01-18 16:13:27 +0100

    chore: solve some linting and type errors

--- src/modules/maps/index.tsx
+++ src/modules/maps/index.tsx
@@ -20,7 +20,7 @@ interface MapData {
     filteredCount: number;
     items: [MapItem]
     totalCount: number;
-  }
+  };
 }
 
 interface MapVariables {
@@ -28,23 +28,33 @@ interface MapVariables {
   limit?: number;
 }
 
-interface MapsProps { };
+interface MapsProps {
+  history: {
+    replace: (path: string) => void;
+    push: (path: string) => void;
+  };
+  match: {
+    params: {
+      mapId?: string;
+    },
+  };
+}
 
 const Wrapper = styled.div`
   display: flex;
 `;
 
-class MapsQuery extends Query<MapData, MapVariables> { };
+class MapsQuery extends Query<MapData, MapVariables> { }
 
 const Maps: React.SFC<MapsProps> = ({ history, match: { params: { mapId: selectedMap } } }) => {
   const hasMapIdSelected = Boolean(selectedMap) && selectedMap !== 'create';
-  const selectMap = React.useCallback((id: string) => history.replace(`/maps/${id}`));
+  const selectMap = React.useCallback((id: string) => history.replace(`/maps/${id}`), []);
   return (
     <React.Fragment>
       <MapsQuery fetchPolicy="cache-and-network" query={mapsQuery}>
         {({ loading, data, error }) => {
-          if (loading) return <Spinner />
-          if (error) return <p>Error {error.message}</p>
+          if (loading) return <Spinner />;
+          if (error) return <p>Error {error.message}</p>;
           return data && (
             <Wrapper>
               <SideBar
@@ -61,7 +71,7 @@ const Maps: React.SFC<MapsProps> = ({ history, match: { params: { mapId: selecte
       </MapsQuery>
       <Route path="/maps/create" component={CreateMapModal} />
     </React.Fragment>
-  )
-}
+  );
+};
 
 export default Maps;
