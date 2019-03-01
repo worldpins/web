5529426cc2a400732aae0f5eeaaee8287db303bf
jdecroock <decroockjovi@gmail.com>
2019-03-01 18:13:15 +0100

    chore: upload modal

---
+++ src/modules/maps/upload/index.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import Dropzone from 'react-dropzone';
+import Papa from 'papaparse';
+
+import Modal from '../../../common/modal';
+import parseData from './_csvParser';
+
+const UploadMap = ({ mutate }) => {
+  const onDrop = React.useCallback(
+    ([file]) => {
+      Papa.parse(file, {
+        complete: async ({ data }) => {
+          const pins = parseData(data);
+          await mutate({
+            variables: {
+              map: {
+                name: 'Temp',
+                pins,
+              },
+            },
+          });
+        },
+        skipEmptyLines: true,
+      });
+    },
+    []);
+  return (
+    <Modal
+      isOpen
+    >
+      <Dropzone onDrop={onDrop}>
+      {({ getRootProps, getInputProps, isDragActive }) => {
+        return (
+          <div
+            {...getRootProps()}
+            className={`dropzone ${isDragActive ? 'dropzone--isActive' : ''}`}
+          >
+            <input {...getInputProps()} />
+            {isDragActive ?
+              <p>Drop files here...</p> :
+              <p>Try dropping some files here, or click to select files to upload.</p>
+            }
+          </div>
+        );
+      }}
+      </Dropzone>
+    </Modal>
+  );
+};
+
+export default UploadMap;
