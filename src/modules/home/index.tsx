364fa0a9abfd9bf2b4b4e2e405298f0a343afa63
jdecroock <decroockjovi@gmail.com>
2019-03-29 18:55:45 +0100

    chore: map Route

--- src/modules/home/index.tsx
+++ src/modules/home/index.tsx
@@ -2,17 +2,23 @@ import * as React from 'react';
 
 import Spinner from '../../common/Spinner';
 import { Route } from 'react-router';
+import styled from '../../layout/styled';
 
 const PublicMaps = React.lazy(() => import('./maps'));
+const PublicMap = React.lazy(() => import('./map'));
+
+const Container = styled.div`
+  padding: 24px 32px;
+`;
 
 const Home = () => (
-  <div>
+  <Container>
     <h1>Welcome to Worldpins!</h1>
     <React.Suspense fallback={<Spinner />}>
       <PublicMaps />
-      <Route path="/:mapId" component={PublicMap} />
+      <Route path="/home/:mapId" component={PublicMap} />
     </React.Suspense>
-  </div>
+  </Container>
 );
 
 export default Home;
