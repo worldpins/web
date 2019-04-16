86aed65a414d3373795aeaba652de5c0f361f694
Jovi De Croock <decroockjovi@gmail.com>
2019-04-16 17:37:39 +0200

    chore: add gql typings

--- webpack.typings.d.ts
+++ webpack.typings.d.ts
@@ -7,6 +7,8 @@ declare module "*.gql" {
   export const maps: any;
   export const createTemplatePin: any;
   export const updateTemplatePin: any;
+  export const login: any;
+  export const register: any;
   export default content;
 }
 
