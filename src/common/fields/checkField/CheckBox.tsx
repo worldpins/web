2fc4c18d0b5ec74be181ee98caaef30f6adb9201
Jovi De Croock <decroockjovi@gmail.com>
2019-04-17 22:09:19 +0200

    chore: update CheckBox

--- src/common/fields/checkField/CheckBox.tsx
+++ src/common/fields/checkField/CheckBox.tsx
@@ -6,13 +6,13 @@ const HiddenCheckbox = styled.input.attrs(checkAttrs)<{ checked?: boolean }>`
   border: 0;
   clip: rect(0 0 0 0);
   clip-path: inset(50%);
-  height: 1px;
-  margin: -1px;
+  height: 0px;
+  margin: 0px;
   overflow: hidden;
   padding: 0;
   position: absolute;
   white-space: nowrap;
-  width: 1px;
+  width: 0px;
 `;
 
 const Icon = styled.svg`
@@ -22,6 +22,7 @@ const Icon = styled.svg`
 `;
 
 const StyledCheckbox = styled.div<{ checked?: boolean }>`
+  cursor: pointer;
   display: inline-block;
   width: 16px;
   height: 16px;
@@ -36,17 +37,23 @@ const StyledCheckbox = styled.div<{ checked?: boolean }>`
   }
 `;
 
-const CheckboxContainer = styled.div``;
+const CheckboxContainer = styled.div`
+  width: 16px;
+  height: 16px;
+`;
 
 interface Props {
   className?: string;
   checked?: boolean;
+  onChange?: (e: React.SyntheticEvent) => void;
 }
 
-const Checkbox: React.FC<Props> = ({ className, checked, ...props }) => (
+const noop = () => {}
+
+const Checkbox: React.FC<Props> = ({ className, checked, onChange, ...props }) => (
   <CheckboxContainer className={className}>
-    <HiddenCheckbox checked={checked} {...props} />
-    <StyledCheckbox checked={checked}>
+    <HiddenCheckbox checked={checked} onChange={noop} {...props} />
+    <StyledCheckbox checked={checked} onClick={onChange}>
     <Icon viewBox="0 0 24 24">
       <polyline points="20 6 9 17 4 12" />
     </Icon>
