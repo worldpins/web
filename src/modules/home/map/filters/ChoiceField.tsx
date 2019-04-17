8ae476d0cf172ed99db06a35be536f6293bad363
Jovi De Croock <decroockjovi@gmail.com>
2019-04-17 22:09:48 +0200

    feat: choicefield

---
+++ src/modules/home/map/filters/ChoiceField.tsx
@@ -0,0 +1,67 @@
+import * as React from 'react';
+import CheckBox from '../../../../common/fields/checkField/CheckBox';
+import styled from '../../../../layout/styled';
+import { useToggle } from 'react-angler';
+
+const Wrapper = styled.div`
+  align-items: center;
+  display: flex;
+  > p {
+    margin: 0;
+    margin-left: 6px;
+  }
+`;
+
+const ChoicesWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 6px;
+`;
+
+interface Props {
+  choices: string[];
+  name: string;
+  setFilters: Function;
+  value: string[];
+}
+
+const ChoiceField: React.FC<Props> = ({ name, choices, setFilters, value }) => {
+  const { value: expanded, toggle } = useToggle(false);
+  const setFilter = React.useCallback((value, toRemove: boolean) => {
+    setFilters((current: any) => {
+      const currentValue = current[name] || [];
+      if (toRemove) {
+        const newValue = currentValue.filter((y: any) => y!==value);
+        if (newValue.length===0) {
+          const { [name]: toDel, ...rest } = current;
+          return rest;
+        }
+        return {
+          ...current,
+          [name]: newValue,
+        }
+      } else {
+        return {
+          ...current,
+          [name]: [...currentValue, value],
+        }
+      }
+    });
+  }, []);
+  return (
+    <div>
+      <h3 onClick={toggle}>{name}</h3>
+      {expanded &&
+        <ChoicesWrapper>
+          {choices.map((x: string) => (
+            <Wrapper key={x}>
+              <CheckBox checked={value.includes(x)} onChange={setFilter.bind(undefined, x, value.includes(x))} />
+              <p>{x}</p>
+            </Wrapper>
+          ))}
+        </ChoicesWrapper>}
+    </div>
+  )
+}
+
+export default ChoiceField;
