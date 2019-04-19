74c03bbec2e9dee34c7a9a74883529c00923dcc5
Jovi De Croock <decroockjovi@gmail.com>
2019-04-19 13:40:39 +0200

    reset filter option

--- src/modules/home/map/filters/index.tsx
+++ src/modules/home/map/filters/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import ChoiceField from './ChoiceField';
 import NumericFilter from './NumericFilter';
+import Button from '../../../../common/button';
 
 interface Props {
   activeFilters: object;
@@ -9,16 +10,34 @@ interface Props {
 }
 
 const Filters: React.FC<Props> = ({ activeFilters, filters, setFilters }) => {
+  const restFilters = React.useCallback(() => {
+    setFilters(() => {});
+  }, []);
+  const { choiceKeys, numericKeys } = React.useMemo(() => {
+    return Object.keys(filters).reduce((acc, key) => {
+      if (filters[key].type === 'choice') {
+        acc.choiceKeys.push(key);
+      } else {
+        acc.numericKeys.push(key);
+      }
+      return acc;
+    }, { choiceKeys: [], numericKeys: [] });
+  }, []);
   return (
     <React.Fragment>
-      {Object.keys(filters).map((property: string) => {
-        const { type, choices, min, max } = filters[property];
-        switch (type) {
-          case 'choice': return <ChoiceField key={property} value={activeFilters[property] || []} name={property} choices={choices} setFilters={setFilters}  />;
-          case 'numeric': return <NumericFilter key={property} name={property} min={min} max={max} setFilters={setFilters} value={activeFilters[property]} />;
-          default: return <span key={property} />;
-        }
-      })}
+      <div>
+        {choiceKeys.map((property: string) => {
+          const { choices } = filters[property];
+          return <ChoiceField key={property} value={activeFilters[property] || []} name={property} choices={choices} setFilters={setFilters}  />;
+        })}
+      </div>
+      <div>
+        {numericKeys.map((property: string) => {
+          const { min, max } = filters[property];
+          return <NumericFilter key={property} name={property} min={min} max={max} setFilters={setFilters} value={activeFilters[property]} />;
+        })}
+      </div>
+      <Button onClick={restFilters} label="Reset" />
     </React.Fragment>
   )
 }
