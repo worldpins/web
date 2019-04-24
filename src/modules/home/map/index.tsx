5956a8101074009903af311124efeacd2ee55f9d
jdecroock <decroockjovi@gmail.com>
2019-04-24 17:05:33 +0200

    styles: left filters right map

--- src/modules/home/map/index.tsx
+++ src/modules/home/map/index.tsx
@@ -5,6 +5,7 @@ import { Query } from 'react-apollo';
 import { Map, TileLayer } from 'react-leaflet';
 import PinMarker from '../../maps/read/Pin';
 import Filters from './filters';
+import styled from '../../../layout/styled';
 
 const attribution = '&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors';
 
@@ -24,6 +25,20 @@ interface Pin {
   orderedFields: string[];
 }
 
+const Wrapper = styled.div`
+  align-items: flex-start;
+  display: flex;
+  margin-left: -32px;
+  margin-right: -32px;
+  padding-left: 6px;
+  padding-right: 6px;
+`;
+
+const MapWrapper = styled.div<{ isOpen: boolean }>`
+  margin-left: 6px;
+  width: ${({ isOpen }) => isOpen ? '80%' : '100%'};
+`;
+
 interface Data {
   publicMap: {
     initialArea: {
@@ -37,24 +52,31 @@ interface Data {
   };
 }
 
-const filterPins = (filterKeys: string[], pins: object[], activeFilters: object, filters: object) => {
+const filterPins = (
+  filterKeys: string[], pins: object[], activeFilters: object, filters: object,
+) => {
   if (filterKeys.length > 0) {
     return pins.filter(({ data }: { data: object }) => {
       return filterKeys.some((key: string) => {
         const { type } = filters[key];
-        if (type==='choice') {
-          const point = data[key].toLowerCase();
-          return activeFilters[key].some((value: string) => point.includes(value));
-        } else if (type==='numeric') {
-          const point = Number(data[key]);
-          return point < (Number(activeFilters[key].max) || Infinity) && point < (Number(activeFilters[key].min) || 0)
+        if (type === 'choice') {
+          const p = data[key];
+          return activeFilters[key].some((value: string) => p.includes(value));
         }
-      })
+        let point = data[key];
+        if (point.includes('to')) {
+          const [min, max] = point.split('to');
+          return Number(max) <= (Number(activeFilters[key].max) || Infinity) &&
+            Number(min) >= (Number(activeFilters[key].min) || 0);
+        }
+        point = Number(point);
+        return point <= (Number(activeFilters[key].max) || Infinity) &&
+          point >= (Number(activeFilters[key].min) || 0);
+      });
     });
-  } else {
-    return pins;
   }
-}
+  return pins;
+};
 
 const MapView: React.FC<RouteComponentProps<{ mapId: string }>> = (
   { match: { params: { mapId } } },
@@ -69,34 +91,36 @@ const MapView: React.FC<RouteComponentProps<{ mapId: string }>> = (
         const { initialArea: { latitude, longitude }, pins, filters } = data!.publicMap;
         const filteredPins = filterPins(filterKeys, pins, activeFilters, filters);
         return (
-          <div>
+          <Wrapper>
             <Filters filters={filters} setFilters={setFilters} activeFilters={activeFilters} />
-            <Map
-              animate
-              center={{ lat: latitude, lng: longitude }}
-              zoom={13}
-            >
-              <TileLayer
-                attribution={attribution}
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              />
-              {filteredPins.map(
-                  ({ comment, id, name, location, data: pinData, orderedFields }: Pin) =>
-                location.latitude ? (
-                  <PinMarker
-                    key={id}
-                    editable={false}
-                    data={pinData}
-                    name={name}
-                    id={id}
-                    location={location}
-                    comment={comment}
-                    orderedFields={orderedFields}
-                  />
-                ) : <React.Fragment key={id} />)
-              }
-            </Map>
-          </div>
+            <MapWrapper isOpen>
+              <Map
+                animate
+                center={{ lat: latitude, lng: longitude }}
+                zoom={13}
+              >
+                <TileLayer
+                  attribution={attribution}
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                {filteredPins.map(
+                    ({ comment, id, name, location, data: pinData, orderedFields }: Pin) =>
+                  location.latitude ? (
+                    <PinMarker
+                      key={id}
+                      editable={false}
+                      data={pinData}
+                      name={name}
+                      id={id}
+                      location={location}
+                      comment={comment}
+                      orderedFields={orderedFields}
+                    />
+                  ) : <React.Fragment key={id} />)
+                }
+              </Map>
+            </MapWrapper>
+          </Wrapper>
         );
       }}
     </Query>
