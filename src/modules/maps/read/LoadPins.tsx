e5b8b9a523494629e68672fee76ba3579beebbeb
Jovi De Croock <decroockjovi@gmail.com>
2019-05-18 10:24:13 +0200

    feat: loading pins

---
+++ src/modules/maps/read/LoadPins.tsx
@@ -0,0 +1,61 @@
+import L from 'leaflet';
+
+export const ColivingMarker = L.icon({
+  iconUrl: require('../../../common/assets/coliving.png'),
+  iconRetinaUrl: require('../../../common/assets/coliving.png'),
+  iconAnchor: [5, 55],
+  popupAnchor: [10, -44],
+  iconSize: [25, 41],
+  shadowSize: [68, 95],
+  shadowAnchor: [20, 92],
+});
+
+export const CohousingMarker = L.icon({
+  iconUrl: require('../../../common/assets/cohousing.png'),
+  iconRetinaUrl: require('../../../common/assets/cohousing.png'),
+  iconAnchor: [5, 55],
+  popupAnchor: [10, -44],
+  iconSize: [25, 41],
+  shadowSize: [68, 95],
+  shadowAnchor: [20, 92],
+});
+
+export const CommuneMarker = L.icon({
+  iconUrl: require('../../../common/assets/commune.png'),
+  iconRetinaUrl: require('../../../common/assets/commune.png'),
+  iconAnchor: [5, 55],
+  popupAnchor: [10, -44],
+  iconSize: [25, 41],
+  shadowSize: [68, 95],
+  shadowAnchor: [20, 92],
+});
+
+export const HomeshareMarker = L.icon({
+  iconUrl: require('../../../common/assets/homeshare.png'),
+  iconRetinaUrl: require('../../../common/assets/homeshare.png'),
+  iconAnchor: [5, 55],
+  popupAnchor: [10, -44],
+  iconSize: [25, 41],
+  shadowSize: [68, 95],
+  shadowAnchor: [20, 92],
+});
+
+export const SharehouseMarker = L.icon({
+  iconUrl: require('../../../common/assets/sharehouse.png'),
+  iconRetinaUrl: require('../../../common/assets/sharehouse.png'),
+  iconAnchor: [5, 55],
+  popupAnchor: [10, -44],
+  iconSize: [25, 41],
+  shadowSize: [68, 95],
+  shadowAnchor: [20, 92],
+});
+
+export const StudentcoopMarker = L.icon({
+  iconUrl: require('../../../common/assets/studentcoop.png'),
+  iconRetinaUrl: require('../../../common/assets/studentcoop.png'),
+  iconAnchor: [5, 55],
+  popupAnchor: [10, -44],
+  iconSize: [25, 41],
+  shadowSize: [68, 95],
+  shadowAnchor: [20, 92],
+});
